96cea47ab60cb5fe5066148dacf67c0b45ba5e27
engektiar <eng.ektiar@gmail.com>
2023-05-14 20:40:59 +0600

    Header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Header.css";
+import logo from "../../assets/logo/FoodTime.png";
+import { BsFillCartFill, BsChevronDown } from "react-icons/bs";
+const Header = () => {
+  return (
+    <div>
+      <Navbar className="headermain">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="misiing" />{" "}
+          </Navbar.Brand>
+          <Nav className="auto">
+            <div className="manuItem">
+              <Nav.Link className="navlink" href="#home">
+                Home
+              </Nav.Link>
+              <Nav.Link className="navlink" href="#features">
+                Blog
+              </Nav.Link>
+              <Nav.Link className="navlink" href="#pricing">
+                About Us
+              </Nav.Link>
+              <Nav.Link className="navlink" href="#pricing">
+                En <BsChevronDown />
+              </Nav.Link>
+            </div>
+            <Nav.Link href="#pricing">
+              <button className="loginbtn">Login</button>
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              <BsFillCartFill />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
